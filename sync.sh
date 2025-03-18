8b0cd490b2bbf7e42c0f5c1ee454fda4b9af8127
Tye Jones (t3) <tye@begottenmedia.com>
2025-03-18 11:20:30 +0100

    only sync when dst dir is missing

--- sync.sh
+++ sync.sh
@@ -4,14 +4,19 @@
 
 source "$GTRPAL_DIR"/tools/sh/__init__.sh
 
-log "synchronizing nuklear src with os/gui/nuklear..."
+if [[ ! -d "$GTRPAL_DIR/src/lib/nuklear" ]]; then
+    log "synchronizing nuklear src with os/gui/nuklear..."
 
-tcd "$GTRPAL_DIR/src/lib/nuklear"
+    mkdir -p "$GTRPAL_DIR/src/lib/nuklear"
+    tcd "$GTRPAL_DIR/src/lib/nuklear"
 
-# if [[ -d "$GTRPAL_DIR/src/os/gui/nuklear" ]]; then
-#     info "removing existing os/gui/nuklear dir"
-#     rm -rf "$GTRPAL_DIR/src/os/gui/nuklear"
-# fi
+    # if [[ -d "$GTRPAL_DIR/src/os/gui/nuklear" ]]; then
+    #     info "removing existing os/gui/nuklear dir"
+    #     rm -rf "$GTRPAL_DIR/src/os/gui/nuklear"
+    # fi
 
-# rsync -vr --exclude="*.md" --exclude="*.py" --exclude="*.sh" --exclude="*.bat" --exclude="*.txt" --exclude="CHANGELOG" --exclude="CREDITS" --exclude="LICENSE" ./src/ "$GTRPAL_DIR/src/os/gui/nuklear"
+    rsync -vr --exclude="*.md" --exclude="*.py" --exclude="*.sh" --exclude="*.bat" --exclude="*.txt" --exclude="CHANGELOG" --exclude="CREDITS" --exclude="LICENSE" ./src/ "$GTRPAL_DIR/src/os/gui/nuklear"
+else
+    log "nuklear src already synchronized"
+fi
 
